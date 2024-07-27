2c8eba55af4090774e6f8fd0d92303ec8b4172f6
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2024-07-27 14:33:38 -0400

    Add files via upload
    adapted tilemovemouse.c patch to dwm 6.5, using this instead of placemouse patch by bakkaby for now.

--- dwm.c
+++ dwm.c
@@ -255,6 +255,7 @@ static void movemouse(const Arg *arg);
 static void moveorplace(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void placemouse(const Arg *arg);
+static void tilemovemouse(const Arg *arg);
 static void pop(Client *c);
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
@@ -1916,8 +1917,8 @@ placemouse(const Arg *arg)
 				selmon = m;
 
 			if (arg->i == 1) { // tiled position is relative to the client window center point
-				px = nx + wa.width / 2;
-				py = ny + wa.height / 2;
+				px = nx + wa.width / 8;
+				py = ny + wa.height / 8;
 			} else { // tiled position is relative to the mouse cursor
 				px = ev.xmotion.x;
 				py = ev.xmotion.y;
@@ -3864,4 +3865,77 @@ bstack(Monitor *m)
 			if (tx + WIDTH(c) + m->pertag->gap[m->pertag->curtag]->gappx < m->mw)
 				tx += WIDTH(c) + m->pertag->gap[m->pertag->curtag]->gappx;
 		}
+}
+
+#define INRECT(X,Y,RX,RY,RW,RH) ((X) >= (RX) && (X) < (RX) + (RW) && (Y) >= (RY) && (Y) < (RY) + (RH))
+
+void
+insertbefore(Client *a, Client *b)	/* insert a before b in the client list */
+{
+	Monitor *m = a->mon;
+	Client **x = &m->clients;
+	
+	while(*x != b && *x)
+		x = & (*x)->next;
+	*x = a;
+	a->next = b;
+}
+
+void
+insertafter(Client *a, Client *b)	/* insert a after b in the client list */
+{
+	a->next = b->next;
+	b->next = a;
+}
+
+void
+tilemovemouse(const Arg *arg) {
+	/* Could EnterNotify events be used instead? */
+	Client *c, *d;
+	Monitor *m;
+	XEvent ev;
+	int x, y;
+	Bool after;
+
+	if(!(c = selmon->sel))
+		return;
+
+	if((m = recttomon(c->x, c->y, c->w, c->h)) != selmon) {
+		sendmon(c, m);
+		selmon = m;
+		focus(NULL);
+	}
+
+	if(c->isfloating || !selmon->lt[selmon->sellt]->arrange){
+		movemouse(NULL);
+		return;
+	}
+	if(XGrabPointer(dpy, root, False, MOUSEMASK, GrabModeAsync, GrabModeAsync,
+	None, cursor[CurMove]->cursor, CurrentTime) != GrabSuccess)
+		return;
+	do {
+		XMaskEvent(dpy, MOUSEMASK|ExposureMask|SubstructureRedirectMask, &ev);
+		switch (ev.type) {
+		case ConfigureRequest:
+		case Expose:
+		case MapRequest:
+			handler[ev.type](&ev);
+			break;
+		case MotionNotify:
+			x = ev.xmotion.x;
+			y = ev.xmotion.y;
+			after = False;
+			for(d = nexttiled(m->clients); d; d = nexttiled(d->next)){
+				if(d == c)
+					after = True;
+				else if(INRECT(x, y, d->x, d->y, d->w+2*borderpx, d->h+2*borderpx)){
+					detach(c);
+					after ? insertafter(c, d) : insertbefore(c,d);
+					arrange(c->mon);
+					break;
+				}
+			}
+		}
+	} while(ev.type != ButtonRelease);
+	XUngrabPointer(dpy, CurrentTime);
 }
