c8cf773c3a2ca66c07f53d08647936361720477b
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2025-03-17 21:13:02 -0400

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -4,7 +4,5 @@
 #xrandr -s 1920x1200 &
 #xset r rate 300 59 &
 #$HOME/.local/bin/wallpaper &
-fastcompmgr &
-#picom &
-killall dwmblocks
-sleep 0.3 && dwmblocks &
+picom &
+( sudo killall dwmblocks ; sleep 1 ; dwmblocks ) &
